af2d453c52f219fd3cf2b1384ccb1e97e61216d4
Jernickjoshi <jernickjoshi01@gmail.com>
2026-07-10 10:27:19 +0530

    initial commit: fitness tracker

---
+++ backend/server.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const app = express();
+app.use(express.json());
+const cors = require('cors');
+app.use(cors());
+const PORT = 3000;
+const { createClient } = require('@supabase/supabase-js');
+const supabaseUrl = 'https://nbmzlsmlmzffvzffquuf.supabase.co';
+const supabaseKey = 'sb_publishable_ALvvrJD_ymKOAGfB9ylyVg_F3D4FnE-';
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+// Our first API Endpoint (The "Read" in CRUD)
+app.get('/api/workouts', async (req, res) => {
+    const { data, error } = await supabase.from('workouts').select('*');
+    if (error) {
+        console.error('Error fetching workouts:', error);
+        res.status(500).json({ error: 'Internal Server Error' });
+        return;
+    }
+    res.json(data);
+
+});
+
+app.post('/api/workouts', async (req, res) => {
+    const newWorkout  = req.body;
+    const { data, error } = await supabase.from('workouts').insert([newWorkout]).select();
+    if(error) {
+        console.error('Error inserting workout:', error);
+        res.status(500).json({ error: 'Internal Server Error' });
+        return;
+    }
+    res.status(201).json(data);
+})
+
+app.delete('/api/workouts/:id', async (req, res) => {
+    const workoutId = req.params.id;
+    const { data, error } = await supabase.from('workouts').delete().eq('id', workoutId).select();
+    if (error) {
+        console.error('Error deleting workout:', error);
+        res.status(500).json({error: 'Internal server error'});
+        return;
+    }
+    res.status(200).json(data);
+});
+app.put('/api/workouts/:id', async (req, res) => {
+    const workoutId = req.params.id;
+    const updatedWorkoutId = req.body;
+    const { data, error } = await supabase.from('workouts').update(updatedWorkoutId).eq('id', workoutId).select();
+    if (error) {
+        console.error('Error updating workout:', error);
+        res.status(500).json({error: 'Internal server error'});
+        return;
+    }
+    res.status(200).json(data);
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on ${PORT}`);   
+});
---
+++ frontend/src/App.jsx
@@ -0,0 +1,134 @@
+import { useState, useEffect } from "react";
+import "./App.css";
+
+const API_URL = "http://localhost:3000/api/workouts";
+function App() {
+  const [workouts, setWorkouts] = useState([]);
+  const [date, setDate] = useState("");
+  const [routineType, setRoutineType] = useState("Push");
+  const [durationTime, setDurationTime] = useState("");
+  const [weight, setWeight] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (date === "" || durationTime === "" || weight === "") {
+      setErrorMessage("Please fill out all fields");
+      return;
+    }
+
+    const newWorkout = {
+      date: date,
+      routine_type: routineType,
+      duration_minutes: durationTime,
+      body_weight_kg: weight,
+    };
+    const response = await fetch(API_URL, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(newWorkout),
+    });
+    const responseData = await response.json();
+    setWorkouts([...workouts, responseData[0]]);
+    setDate("");
+    setRoutineType("Push");
+    setDurationTime("");
+    setWeight("");
+  };
+  const handleDelete = async (id) => {
+    await fetch(`${API_URL}/${id}`, {
+      method: "DELETE",
+    });
+    setWorkouts(workouts.filter((workout) => workout.id != id));
+  };
+  const handleUpdate = async (workout) => {
+    const newWeight = prompt("Enter new body weight: ", workout.body_weight_kg);
+    const updatedWorkout = { ...workout, body_weight_kg: newWeight };
+    await fetch(`${API_URL}/${workout.id}`, {
+      method: "PUT",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(updatedWorkout),
+    });
+    setWorkouts(
+      workouts.map((w) => (w.id === workout.id ? updatedWorkout : w)),
+    );
+  };
+  useEffect(() => {
+    fetch(API_URL)
+      .then((response) => response.json())
+      .then((data) => {
+        setWorkouts(data);
+      })
+      .catch((error) => console.log("Error fetching workouts:", error));
+  }, []);
+  return (
+    <div>
+      <h1>My Fitness Tracker</h1>
+      <form onSubmit={handleSubmit} className="workout-form">
+        <input
+          className="input-field"
+          type="date"
+          placeholder="Date"
+          value={date}
+          onChange={(e) => {
+            setDate(e.target.value);
+            setDate(setErrorMessage(""));
+          }}
+        />
+        <select
+          className="input-field"
+          value={routineType}
+          onChange={(e) => {
+            setRoutineType(e.target.value);
+            setRoutineType(setErrorMessage(""));
+          }}
+        >
+          <option value="push">Push</option>
+          <option value="pull">Pull</option>
+          <option value="legs">Legs</option>
+        </select>
+        <input
+          className="input-field"
+          type="number"
+          placeholder="Duration (minutes)"
+          value={durationTime}
+          onChange={(e) => {
+            setDurationTime(e.target.value);
+            setDurationTime(setErrorMessage(""));
+          }}
+        />
+        <input
+          className="input-field"
+          type="number"
+          placeholder="Body Weight (kg)"
+          value={weight}
+          onChange={(e) => {
+            setWeight(e.target.value);
+            setWeight(setErrorMessage(""));
+          }}
+        />
+        <button type="submit" className="submit-btn">
+          Add Workout
+        </button>
+        <p className="error-message">{errorMessage}</p>
+      </form>
+      {workouts.map((workout) => (
+        <div
+          key={workout.id}
+          style={{ border: "1px solid grey", margin: "10px", padding: "10px" }}
+        >
+          <h2>{workout.routine_type} Day</h2>
+          <p>{workout.date}</p>
+          <p>{workout.duration_minutes} Minutes</p>
+          <p>{workout.body_weight_kg} kg</p>
+          <button onClick={() => handleDelete(workout.id)} className="map-btn">
+            Delete Workout
+          </button>
+          <button onClick={() => handleUpdate(workout)} className="map-btn">
+            Update weight
+          </button>
+        </div>
+      ))}
+    </div>
+  );
+}
+export default App;
