6eb642c5362731e06c3c7ea47fb909755f38db1b
Jernickjoshi <jernickjoshi01@gmail.com>
2026-07-11 11:28:10 +0530

    new chart featured added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,14 @@
 import { useState, useEffect } from "react";
 import "./App.css";
-
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  ResponsiveContainer,
+} from "recharts";
 const API_URL = "https://fitness-tracker-api-z3i9.onrender.com/api/workouts";
 function App() {
   const [workouts, setWorkouts] = useState([]);
@@ -60,6 +68,9 @@ function App() {
       })
       .catch((error) => console.log("Error fetching workouts:", error));
   }, []);
+  const sortedWorkouts = [...workouts].sort(
+    (a, b) => new Date(a.date) - new Date(b.date),
+  );
   return (
     <div>
       <h1>My Fitness Tracker</h1>
@@ -111,6 +122,44 @@ function App() {
         </button>
         <p className="error-message">{errorMessage}</p>
       </form>
+      {workouts.length > 0 && (
+        <div>
+          <h1>Weight Progress Chart</h1>
+          <ResponsiveContainer width="100%" height={300}>
+            <LineChart data={sortedWorkouts}>
+              <CartesianGrid strokeDasharray="3 3" stroke="#333" />
+              <XAxis dataKey="date" stroke="#fff" tick={{ fill: "#fff" }} />
+              <YAxis
+                stroke="#fff"
+                tick={{ fill: "#fff" }}
+                domain={["dataMin - 2", "dataMax + 2"]}
+              />
+              <Tooltip
+                contentStyle={{
+                  backgroundColor: "#2a2a2a",
+                  border: "1px solid #1db954",
+                  borderRadius: "8px",
+                  color: "#fff",
+                }}
+                itemStyle={{ color: "#1db954" }}
+              />
+              <Line
+                type="monotone"
+                dataKey="body_weight_kg"
+                name="Weight(kg)"
+                stroke="#1db954"
+                strokeWidth={3}
+                dot={{
+                  r: 5,
+                  fill: "#121212",
+                  stroke: "#1db954",
+                  strokeWidth: 2,
+                }}
+              />
+            </LineChart>
+          </ResponsiveContainer>
+        </div>
+      )}
       {workouts.map((workout) => (
         <div
           key={workout.id}
